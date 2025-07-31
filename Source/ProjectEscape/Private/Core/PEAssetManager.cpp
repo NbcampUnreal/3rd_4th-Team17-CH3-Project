d16f1d160da13f80c09a6b017d237247a688dc77
DaegeunChoe <asdf12gg@gmail.com>
2025-07-31 15:38:33 +0900

    PE(Project Escape) 로그 채널 추가

--- Source/ProjectEscape/Private/Core/PEAssetManager.cpp
+++ Source/ProjectEscape/Private/Core/PEAssetManager.cpp
@@ -1,8 +1,11 @@
 #include "Core/PEAssetManager.h"
 #include "Core/PEGameplayTags.h"
+#include "Core/PELogChannels.h"
 
 void UPEAssetManager::StartInitialLoading()
 {
 	Super::StartInitialLoading();
 	FPEGameplayTags::InitializeNativeTags();
+
+	UE_LOG(LogPE, Display, TEXT("UPEAssetManager::StartInitialLoading()"));
 }
