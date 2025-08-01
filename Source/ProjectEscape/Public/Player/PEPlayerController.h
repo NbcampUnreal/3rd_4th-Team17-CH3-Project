cb5a39fdd4dd71dced8bd34b6e93211c9ed34a26
DaegeunChoe <asdf12gg@gmail.com>
2025-08-01 16:41:31 +0900

    기본 Gameplay Framework 클래스 상속

---
+++ Source/ProjectEscape/Public/Player/PEPlayerController.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PEPlayerController.generated.h"
+
+UCLASS()
+class PROJECTESCAPE_API APEPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
