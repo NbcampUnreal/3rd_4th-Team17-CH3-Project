3fbcfe72766272b91093d2aa50515c4c5cb464b9
DaegeunChoe <asdf12gg@gmail.com>
2025-08-06 20:02:47 +0900

    APEUITestPlayerController 의 코드를 실제 게임에 사용할 수 있도록, 코드를 복사&붙여넣기 했습니다.

--- Source/ProjectEscape/Private/Player/PEStartGamePlayerController.cpp
+++ Source/ProjectEscape/Private/Player/PEStartGamePlayerController.cpp
@@ -1,2 +1,97 @@
 #include "Player/PEStartGamePlayerController.h"
+#include "Blueprint/UserWidget.h"
 
+void APEStartGamePlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	ShowMainMenu();
+}
+
+void APEStartGamePlayerController::OnClickCredit()
+{
+	ClearAllWidget();
+
+	if (!CreditWidget && CreditWidgetClass)
+	{
+		CreditWidget = CreateWidget<UUserWidget>(this, CreditWidgetClass);
+	}
+
+	if (CreditWidget && !CreditWidget->IsInViewport())
+	{
+		CreditWidget->AddToViewport();
+		SetInputMode(FInputModeUIOnly());
+		bShowMouseCursor = true;
+		SetPause(true);
+	}
+}
+
+void APEStartGamePlayerController::OnClickHelper()
+{
+	ClearAllWidget();
+
+	if (!HelperWidget && HelperWidgetClass)
+	{
+		HelperWidget = CreateWidget<UUserWidget>(this, HelperWidgetClass);
+	}
+
+	if (HelperWidget && !HelperWidget->IsInViewport())
+	{
+		HelperWidget->AddToViewport();
+		SetInputMode(FInputModeUIOnly());
+		bShowMouseCursor = true;
+		SetPause(true);
+	}
+}
+
+void APEStartGamePlayerController::OnClickBackMainMenu()
+{
+	ClearAllWidget();
+
+	if (!MainMenuWidget && MainMenuWidgetClass)
+	{
+		MainMenuWidget = CreateWidget<UUserWidget>(this, MainMenuWidgetClass);
+	}
+
+	if (MainMenuWidget && !MainMenuWidget->IsInViewport())
+	{
+		MainMenuWidget->AddToViewport();
+		SetInputMode(FInputModeUIOnly());
+		bShowMouseCursor = true;
+		SetPause(true);
+	}
+}
+
+void APEStartGamePlayerController::ClearAllWidget()
+{
+	if (MainMenuWidget)
+	{
+		MainMenuWidget->RemoveFromParent();
+		MainMenuWidget = nullptr;
+	}
+	if (CreditWidget)
+	{
+		CreditWidget->RemoveFromParent();
+		CreditWidget = nullptr;
+	}
+	if (HelperWidget)
+	{
+		HelperWidget->RemoveFromParent();
+		HelperWidget = nullptr;
+	}
+}
+
+void APEStartGamePlayerController::ShowMainMenu()
+{
+	if (!MainMenuWidget && MainMenuWidgetClass)
+	{
+		MainMenuWidget = CreateWidget<UUserWidget>(this, MainMenuWidgetClass);
+	}
+
+	if (MainMenuWidget && !MainMenuWidget->IsInViewport())
+	{
+		MainMenuWidget->AddToViewport();
+		SetInputMode(FInputModeUIOnly());
+		bShowMouseCursor = true;
+		SetPause(true);
+	}
+}
--- Source/ProjectEscape/Public/Player/PEStartGamePlayerController.h
+++ Source/ProjectEscape/Public/Player/PEStartGamePlayerController.h
@@ -9,4 +9,32 @@ class PROJECTESCAPE_API APEStartGamePlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	UFUNCTION(BlueprintCallable)
+	void OnClickCredit();
+
+	UFUNCTION(BlueprintCallable)
+	void OnClickHelper();
+
+	UFUNCTION(BlueprintCallable)
+	void OnClickBackMainMenu();
+
+	void ClearAllWidget();
+	void ShowMainMenu();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI|Class")
+	TSubclassOf<UUserWidget> MainMenuWidgetClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI|Class")
+	TSubclassOf<UUserWidget> CreditWidgetClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI|Class")
+	TSubclassOf<UUserWidget> HelperWidgetClass;
+
+	UUserWidget* MainMenuWidget;
+	UUserWidget* CreditWidget;
+	UUserWidget* HelperWidget;
 };
