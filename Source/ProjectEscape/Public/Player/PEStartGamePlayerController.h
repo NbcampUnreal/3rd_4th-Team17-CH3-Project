0b778a8f24809c8e656c4d8b511c34432c9a6fa3
DaegeunChoe <asdf12gg@gmail.com>
2025-08-06 19:36:58 +0900

    Game Start 전용 PlayerController 생성

---
+++ Source/ProjectEscape/Public/Player/PEStartGamePlayerController.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PEStartGamePlayerController.generated.h"
+
+UCLASS()
+class PROJECTESCAPE_API APEStartGamePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
