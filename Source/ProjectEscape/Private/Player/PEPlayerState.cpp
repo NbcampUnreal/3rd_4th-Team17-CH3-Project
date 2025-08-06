a6c0f1aa1eb7066dd39b22e81d2183cbfb11bc46
DaegeunChoe <asdf12gg@gmail.com>
2025-08-06 19:20:53 +0900

    플레이어 상태 정보를 PlayerState으로 이전

--- Source/ProjectEscape/Private/Player/PEPlayerState.cpp
+++ Source/ProjectEscape/Private/Player/PEPlayerState.cpp
@@ -1,2 +1,63 @@
 #include "Player/PEPlayerState.h"
 
+APEPlayerState::APEPlayerState()
+{
+	HealthPoint = MaxHealthPoint;
+	Stamina = MaxStamina;
+}
+
+void APEPlayerState::SetHealthPoint(float NewValue)
+{
+	HealthPoint = FMath::Clamp(NewValue, 0, MaxHealthPoint);
+	OnChangeHealthPoint.Broadcast(HealthPoint, MaxHealthPoint);
+
+	if (HealthPoint <= 0)
+	{
+		// TODO: Implement Hero character death logic
+	}
+}
+
+void APEPlayerState::SetMaxHealthPoint(float NewValue)
+{
+	if (NewValue > 0)
+	{
+		MaxHealthPoint = NewValue;
+		SetHealthPoint(HealthPoint);
+	}
+}
+
+void APEPlayerState::IncreaseHealthPoint(float Value)
+{
+	SetHealthPoint(HealthPoint + Value);
+}
+
+void APEPlayerState::ReduceHealthPoint(float Value)
+{
+	SetHealthPoint(HealthPoint - Value);
+}
+
+void APEPlayerState::SetStamina(float NewValue)
+{
+	Stamina = FMath::Clamp(NewValue, 0, MaxStamina);
+	OnChangeStamina.Broadcast(Stamina, MaxStamina);
+}
+
+void APEPlayerState::SetMaxStamina(float NewValue)
+{
+	if (NewValue > 0)
+	{
+		MaxStamina = NewValue;
+		SetStamina(Stamina);
+	}
+}
+
+void APEPlayerState::IncreaseStamina(float Value)
+{
+	SetStamina(Stamina + Value);
+}
+
+void APEPlayerState::ReduceStamina(float Value)
+{
+	SetStamina(Stamina - Value);
+}
+
--- Source/ProjectEscape/Public/Player/PEPlayerState.h
+++ Source/ProjectEscape/Public/Player/PEPlayerState.h
@@ -4,9 +4,47 @@
 #include "GameFramework/PlayerState.h"
 #include "PEPlayerState.generated.h"
 
+DECLARE_MULTICAST_DELEGATE_TwoParams(FOnFloatStatusChanged, /*Current*/ float, /*Max*/ float)
+
 UCLASS()
 class PROJECTESCAPE_API APEPlayerState : public APlayerState
 {
 	GENERATED_BODY()
-	
+
+public:
+	APEPlayerState();
+
+	/* Status Delegates, Getters, Setters */
+	FOnFloatStatusChanged OnChangeHealthPoint;
+	FOnFloatStatusChanged OnChangeStamina;
+
+	float GetHealthPoint() const { return HealthPoint; }
+	float GetMaxHealthPoint() const { return MaxHealthPoint; }
+
+	float GetStamina() const { return Stamina; }
+	float GetMaxStamina() const { return MaxStamina; }
+
+	void SetHealthPoint(float NewValue);
+	void SetMaxHealthPoint(float NewValue);
+	void IncreaseHealthPoint(float Value);
+	void ReduceHealthPoint(float Value);
+
+	void SetStamina(float NewValue);
+	void SetMaxStamina(float NewValue);
+	void IncreaseStamina(float Value);
+	void ReduceStamina(float Value);
+
+protected:
+	/* Status */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Status")
+	float MaxHealthPoint = 100;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Status")
+	float HealthPoint;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Status")
+	float MaxStamina = 100;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Status")
+	float Stamina;
 };
