ababa987025cdbd322325c96d8da32da954cf7f2
Seonho Kim <dev.sensational@gmail.com>
2025-08-08 19:06:48 +0900

    아이템 태그 추가, 테스트 필드 수정

--- Source/ProjectEscape/Private/Core/PEGameplayTags.cpp
+++ Source/ProjectEscape/Private/Core/PEGameplayTags.cpp
@@ -22,6 +22,8 @@ void FPEGameplayTags::AddAllTags()
 	AddTag(Item_Weapon_SubWeapon, "Item.Weapon.SubWeapon", "");
 	AddTag(Item_Weapon_MeleeWeapon, "Item.Weapon.MeleeWeapon", "");
 	AddTag(Item_Things_Ammo, "Item.Things.Ammo", "");
+	AddTag(Item_Things_Ammo_Rifle, "Item.Things.Ammo.Rifle", "");
+	AddTag(Item_Things_Ammo_Shotgun, "Item.Things.Ammo.Shotgun", "");
 	AddTag(Item_Things_Heal, "Item.Things.Heal", "");
 	AddTag(Item_Things_Grenade, "Item.Things.Grenade", "");
 	AddTag(Item_Things_Key, "Item.Things.Key", "");
