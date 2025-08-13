cc1697eef79af42ea40e0e208ea49c3a78887fc2
DaegeunChoe <asdf12gg@gmail.com>
2025-08-13 14:59:52 +0900

    game over 구현중

--- Source/ProjectEscape/Private/Core/PEGameModeBase.cpp
+++ Source/ProjectEscape/Private/Core/PEGameModeBase.cpp
@@ -1,2 +1,27 @@
 #include "Core/PEGameModeBase.h"
 
+void APEGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void APEGameModeBase::EndPlay(EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+}
+
+void APEGameModeBase::GameOver(APlayerController* PlayerController)
+{
+	if (PlayerController)
+	{
+		if (APawn* Pawn = PlayerController->GetPawn())
+		{
+			Pawn->Destroy();
+		}
+	}
+	
+}
+
+void APEGameModeBase::GameClear(APlayerController* PlayerController)
+{
+}
--- Source/ProjectEscape/Public/Core/PEGameModeBase.h
+++ Source/ProjectEscape/Public/Core/PEGameModeBase.h
@@ -4,9 +4,20 @@
 #include "GameFramework/GameModeBase.h"
 #include "PEGameModeBase.generated.h"
 
+class APlayerController;
+
 UCLASS()
 class PROJECTESCAPE_API APEGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
+
+public:
+	virtual void BeginPlay() override;
+	virtual void EndPlay(EEndPlayReason::Type EndPlayReason) override;
+
+	UFUNCTION(BlueprintCallable)
+	void GameOver(APlayerController* PlayerController);
 	
+	UFUNCTION(BlueprintCallable)
+	void GameClear(APlayerController* PlayerController);
 };
