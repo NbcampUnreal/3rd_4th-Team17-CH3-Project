b8bfcd83463eb7b610c417577a36c1d1dbd4bfb9
prostruin <wkd83555@naver.com>
2025-08-14 18:26:33 +0900

    테스트용 완성코드 및 작업

---
+++ Source/ProjectEscape/Private/UI/HUD/PEUIDamage.cpp
@@ -0,0 +1,65 @@
+﻿
+
+#include "UI/HUD/PEUIDamage.h"
+#include "Components/WidgetComponent.h"
+#include "Components/TextBlock.h"
+#include "Blueprint/UserWidget.h"
+#include "TimerManager.h"
+
+
+APEUIDamage::APEUIDamage()
+{
+	WidgetComp = CreateDefaultSubobject<UWidgetComponent>(TEXT("WidgetComp"));
+	SetRootComponent(WidgetComp);
+
+	WidgetComp->SetWidgetSpace(EWidgetSpace::Screen);
+}
+
+
+
+
+void APEUIDamage::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (DamageWidgetclass)
+	{
+		WidgetComp->SetWidgetClass(DamageWidgetclass);
+	}
+
+	UpdateDamageUI();
+	
+}
+
+void APEUIDamage::UpdateDamageUI()
+{
+	if (UUserWidget* DamageIns = WidgetComp->GetUserWidgetObject())
+	{
+		if (UTextBlock* DamageText = Cast<UTextBlock>(DamageIns->GetWidgetFromName(TEXT("DamageText"))))
+		{
+			DamageText->SetText(FText::FromString(FString::Printf(TEXT("%.0f"), DamageAmount)));
+		}
+	}
+
+	FVector ActorLocation = GetActorLocation();
+	float RandXY = FMath::FRandRange(-25.0f, 25.0f);
+	float RandZ = FMath::FRandRange(25.0f, 85.0f);
+	FVector EndLocation = ActorLocation + FVector(RandXY, RandXY, RandZ);
+	SetActorLocation(EndLocation);
+
+	FTimerHandle TimerHandle;
+	GetWorldTimerManager().SetTimer(
+		TimerHandle,
+		this,
+		&APEUIDamage::DestroySelf,
+		1.5f,
+		false
+	);
+}
+
+void APEUIDamage::DestroySelf()
+{
+	Destroy();
+}
+
+
---
+++ Source/ProjectEscape/Public/UI/HUD/PEUIDamage.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PEUIDamage.generated.h"
+
+class UWidgetComponent;
+class UUserWidget;
+class UTextBlock;
+
+UCLASS()
+class PROJECTESCAPE_API APEUIDamage : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+
+	APEUIDamage();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
+	TSubclassOf<UUserWidget> DamageWidgetclass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+	float DamageAmount = 0.0f;
+
+	void UpdateDamageUI();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Comp")
+	UWidgetComponent* WidgetComp;
+
+	void DestroySelf();
+
+};
