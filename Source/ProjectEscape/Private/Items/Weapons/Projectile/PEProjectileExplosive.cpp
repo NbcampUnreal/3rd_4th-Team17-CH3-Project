27b86efbfefa78f7e0d2bc60229ba96961f0dc13
Seonho Kim <dev.sensational@gmail.com>
2025-08-16 17:12:17 +0900

    폭발데미지 구현

---
+++ Source/ProjectEscape/Private/Items/Weapons/Projectile/PEProjectileExplosive.cpp
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/Weapons/Projectile/PEProjectileExplosive.h"
+
+#include "Kismet/GameplayStatics.h"
+
+
+APEProjectileExplosive::APEProjectileExplosive()
+{
+	
+}
+
+void APEProjectileExplosive::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void APEProjectileExplosive::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+	PerformExplosion();
+}
+
+void APEProjectileExplosive::PerformExplosion()
+{
+	float ExplosionRadius = 500.0f;
+    
+	UGameplayStatics::ApplyRadialDamage(
+		GetWorld(),
+		ProjectileStats.DamageAmount,
+		GetActorLocation(),
+		ExplosionRadius,
+		UDamageType::StaticClass(),
+		TArray<AActor*>(), // 무시할 액터들
+		this,              // 데미지 원인자
+		GetInstigatorController(),
+		true,              // 풀 데미지 적용
+		ProjectileStats.CollisionChannel
+	);
+
+	OnProjectileExpired();
+}
--- Source/ProjectEscape/Public/Items/Weapons/Projectile/PEProjectileBase.h
+++ Source/ProjectEscape/Public/Items/Weapons/Projectile/PEProjectileBase.h
@@ -53,11 +53,10 @@ protected:
 	UPROPERTY()
 	FTimerHandle ProjectileLifetimeTimer;
 
-private:
+protected:
 	UFUNCTION()
-	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+	virtual void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 
 	UFUNCTION()
 	void OnProjectileExpired();
-	
 };
